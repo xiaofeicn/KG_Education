02d5862743543e37be61277daa6a4629084ec80a
xiaofeicn <271986756@qq.com>
2019-10-12 16:12:15 +0800

    Update default.py
    # 针对Pymongo3.9 mongoengine0.18.2 添加验证配置

--- web/app/config/default.py
+++ web/app/config/default.py
@@ -7,9 +7,13 @@ class Config():
     PORT = 9090
     HTML_MINIFY = True
     SECRET_KEY = 'EG_Education_development_key'
+    # 针对Pymongo3.9 mongoengine0.18.2 验证添加
     MONGODB_SETTINGS = {
         'db': 'KG_Education',
         'host': '127.0.0.1',
         'port': 27017,
+        'username': 'root',
+        'password': 'root1234',
+        'authentication_source':'admin'
     }
     
